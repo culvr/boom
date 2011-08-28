ba35f639e68ab4f32353eb59b1054891fb44ffc3
Andrew Culver <andrew@culvr.com>
2011-08-28 13:26:48 -0500

    Create new list with item value from STDIN

--- test/test_command.rb
+++ test/test_command.rb
@@ -65,6 +65,13 @@ class TestCommand < Test::Unit::TestCase
   def test_list_creation_with_item
     assert_match /a new list called newlist.* item in newlist/, command('newlist item blah')
   end
+  
+  def test_list_creation_with_item_from_stdin
+    STDIN.stubs(:read).returns('blah')
+    STDIN.stubs(:stat).returns()
+    STDIN.stat.stubs(:size).returns(4)
+    assert_match /a new list called newlist.* item in newlist is blah/, command("newlist item")
+  end
 
   def test_item_access
     assert_match /copied https:\/\/github\.com to your clipboard/,
